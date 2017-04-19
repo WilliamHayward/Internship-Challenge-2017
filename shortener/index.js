66f9c47c16deaf3775810c5a9b5a16997a4b9c74
William Hayward <haywardwilliam1996@gmail.com>
2017-04-19 11:06:38 +1000

    +Completed URL routing +Fixed error with inconsistency between client and server shortened urls

--- shortener/index.js
+++ shortener/index.js
@@ -4,34 +4,65 @@ if (Meteor.isClient) {
     Template.urlShorten.events({
         'submit form': function() {
             event.preventDefault(); // Stop page from refreshing
-            Meteor.call('shortenURL', event.target.url.value);
+            Meteor.call('shortenURL', event.target.url.value, function(err, shortened) {
+                Session.set("urlOutput", shortened);
+            });
         }
     });
 
     Template.urlShorten.helpers({
         urlOutput: function() {
-            return Session.get('urlOutput');
-        },
+            var shortened = Session.get('urlOutput');
+            if (shortened == undefined) {
+                return "";
+            }
+            return document.domain + "/" + shortened;
+        }
     });
 }
 
 Meteor.methods({
     'shortenURL': function(url) {
-        var alphabet = "abcdefghijxlmnopqrstuvwxyz";
-        var shortened = "";
-        while (shortened == "" || urlList.find({_id: shortened}).fetch().length != 0) {
-            for (var i = 0; i < 6; i++) {
-                var position = Math.random() * alphabet.length;
-                position = Math.floor(position);
-                shortened += alphabet[position];
-            }
+        if (Meteor.isServer) {
+            var alphabet = "abcdefghijxlmnopqrstuvwxyz";
+            var shortened = "";
+            do {
+                for (var i = 0; i < 6; i++) {
+                    var position = Math.random() * alphabet.length;
+                    position = Math.floor(position);
+                    shortened += alphabet[position];
+                }
+            } while (urlList.find({_id: shortened}).fetch().length != 0);
+            urlList.insert({
+                _id: shortened,
+                longURL: url,
+            });
+            return shortened;
         }
-        urlList.insert({
-            _id: shortened,
-            longURL: url
-        });
-        if (Meteor.isClient) {
+        /*if (Meteor.isClient) {
             Session.set('urlOutput', shortened);
+        }*/
+    },
+    'getURL': function(shortened) {
+        if (Meteor.isServer) {
+            var url = urlList.find({
+                _id: shortened
+            }).fetch()[0].longURL;
+            return url;
         }
+    }
+});
+FlowRouter.route('/', {
+    action() {
+        //Default
+    }
+});
+
+FlowRouter.route('/:_shortened', {
+    action() {
+        var shortened = FlowRouter.getParam("_shortened");
+        Meteor.call("getURL", shortened, function(err, url) {
+            window.location = url;
+        })
     }
 })
